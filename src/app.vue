cc9bba9fd9268563c14b1c86436877d8260c3a40
yuyang <yuyang545262477@gmail.com>
2018-02-02 16:54:05 +0800

    init project

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="vue">{{text}}</div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                text: 'hello vue '
+            }
+        }
+    }
+</script>
+<style>
+    #vue {
+        font-size: 36px;
+        color: aqua;
+    }
+</style>
