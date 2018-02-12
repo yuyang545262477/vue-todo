84ff5534e81bf08321325b294055dbb073aa0cc1
David Yu <yuyang545262477@gmail.com>
2018-02-12 18:18:14 +0800

    file hot load

--- src/app.vue
+++ src/app.vue
@@ -1,18 +1,18 @@
 <template>
-    <div id="vue">{{text}}</div>
+	<div id="vue">{{text}}</div>
 </template>
 <script>
     export default {
         data() {
             return {
-                text: 'hello vue '
+                text: 'hello vue 123 456'
             }
         }
     }
 </script>
 <style>
-    #vue {
-        font-size: 36px;
-        color: aqua;
-    }
+	#vue {
+		font-size: 36px;
+		color: aqua;
+	}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const isDev = process.env.NODE_ENV === 'development';
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+const config = {
+    target: "web",
     entry: path.join(__dirname, 'src/index.js'),
     output: {
         filename: 'bundle.js',
@@ -18,23 +22,23 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
-        //    parsing img file
+            //    parsing img file
             {
-                test:/\.(gif|jpg|jpeg|png|svg)$/,
-                use:[
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
                     {
-                        loader:'url-loader',
-                        options:{
-                            limit:1024,
-                            name:'[name]-aaa.[ext]'
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name]-aaa.[ext]'
                         }
                     }
                 ]
             },
-        //    parsing styles
+            //    parsing styles
             {
-                test:/\.styl/,
-                use:[
+                test: /\.styl/,
+                use: [
                     'style-loader',
                     'css-loader',
                     'stylus-loader',
@@ -44,6 +48,34 @@ module.exports = {
             }
 
 
-        ]
-    }
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin()
+    ]
 };
+
+if (isDev) {
+    config.devtool = "#cheap-module-eval-source-map";
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay: {
+            errors: true
+        },
+        hot: true
+    };
+
+    // noinspection JSUnresolvedFunction
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config;
